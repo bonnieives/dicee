68cb64522a844c6e181cc6a6842faab50d559938
Bonnie <bonnieives@gmail.com>
2024-01-30 16:27:49 -0500

    Project done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -23,18 +24,47 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 5;
+  int rightDiceNumber = 3;
+
+  void rollDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6)+1;
+      rightDiceNumber = Random().nextInt(6)+1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset('android/images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('android/images/dice1.png'),
-        ),
-      ],
+        return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+                onPressed: (){
+                  rollDice();
+                },
+                child: Image.asset('android/images/dice$leftDiceNumber.png')
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                rollDice();
+              },
+              child: Image.asset('android/images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
+
